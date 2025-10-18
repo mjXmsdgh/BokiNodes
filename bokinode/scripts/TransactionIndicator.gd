3c73f0632bb4e628872fee8c85374d62fc596eda
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-10-18 19:22:54 +0900

    change format

--- bokinode/scripts/TransactionIndicator.gd
+++ bokinode/scripts/TransactionIndicator.gd
@@ -5,5 +5,6 @@ extends Node2D
 
 
 func set_amount(amount: float):
+	amount_label.text = "%s" % amount
 	# 日本円形式の文字列にフォーマットして表示
-	amount_label.text = "¥{:,}".format({"":"{0}".format([amount])})
+	#amount_label.text = "¥{:,}".format({"":"{0}".format([amount])})
